5a1342150186d430046b6f5c6f6f644adeb7b393
tiarahasnabila <tiarahasnabila@gmail.com>
2025-01-13 12:36:08 +0700

    menambahkan navigasi untuk home dan insert mahasiswa view

--- app/src/main/java/com/example/prak10firebase/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/prak10firebase/navigation/PengelolaHalaman.kt
@@ -6,8 +6,10 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+
 import com.example.prak10firebase.ui.home.pages.HomeScreen
 import com.example.prak10firebase.ui.home.pages.InsertMhsView
+
 
 @Composable
 fun PengelolaHalaman(
