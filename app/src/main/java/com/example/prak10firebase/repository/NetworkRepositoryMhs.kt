691e57e79a1839fe4877292b94f40a1baf581aa1
tiarahasnabila <tiarahasnabila@gmail.com>
2025-01-13 09:50:53 +0700

    menambahkan fungsi insertmhs untuk menyimpan datamhs

--- app/src/main/java/com/example/prak10firebase/repository/NetworkRepositoryMhs.kt
+++ app/src/main/java/com/example/prak10firebase/repository/NetworkRepositoryMhs.kt
@@ -6,40 +6,19 @@ import com.google.firebase.firestore.Query
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
 
 class NetworkRepositoryMhs(
     private val firestore: FirebaseFirestore
 ) : RepositoryMhs {
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
-    }
-
-    override fun getAllMhs(): Flow<List<Mahasiswa>> = callbackFlow {
-        val mhsCollection = firestore.collection("Mahasiswa")
-            .orderBy("nim", Query.Direction.ASCENDING)
-            .addSnapshotListener{ value,error ->
-                if (value != null){
-                    val mhsList = value.documents.mapNotNull{
-                        it.toObject(Mahasiswa::class.java)!!
-                    }
-                    trySend(mhsList)
-                }
-            }
-        awaitClose{
-            mhsCollection.remove()
+        try {
+            firestore.collection("Mahasiswa").add(mahasiswa).await()
+        } catch (e: Exception) {
+            throw Exception(
+                "Gagal menambahkan data mahasiswa: " +
+                        "${e.message}"
+            )
         }
     }
 
-    override fun getMhs(nim: String): Flow<Mahasiswa> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
-    }
-
-}
